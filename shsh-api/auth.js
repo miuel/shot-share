aeaee492cb960c72a99df03fa8d5befbae8422fe
miuel <rivasmig@gmail.com>
2016-09-23 15:36:02 +0100

    actualizando auth.js

---
+++ shsh-api/auth.js
@@ -0,0 +1,48 @@
+'use strict'
+
+const { send, json } = require('micro')
+const HttpHash = require('http-hash')
+const Db = require('shsh-db')
+const config = require('./config')
+const utils = require('./lib/utils')
+const DbStub = require('./test/stub/db')
+
+const env = process.env.NODE_ENV || 'production'
+let db = new Db(config.db)
+
+if (env === 'test') {
+  db = new DbStub()
+}
+
+const hash = HttpHash()
+
+hash.set('POST /', async function authenticate (req, res, params) {
+  let credentials = await json(req)
+  await db.connect()
+  let auth = await db.authenticate(credentials.username, credentials.password)
+  await db.disconnect()
+
+  if (!auth) {
+    return send(res, 401, { error: 'invalid credentials' })
+  }
+  let token = await utils.signToken({
+    userId: credentials.username
+  }, config.secret)
+
+  send(res, 200, token)
+})
+
+module.exports = async function main (req, res) {
+  let { method, url } = req
+  let match = hash.get(`${method.toUpperCase()} ${url}`)
+
+  if (match.handler) {
+    try {
+      await match.handler(req, res, match.params)
+    } catch (e) {
+      send(res, 557, { error: e.message })
+    }
+  } else {
+    send(res, 404, { error: 'route not found' })
+  }
+}
